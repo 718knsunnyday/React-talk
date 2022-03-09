971f542866ac73b365e18e207d0a2b37f56afa6c
KanakoNakano <90448388+718knsunnyday@users.noreply.github.com>
2022-03-09 22:55:41 +0900

    [Add]追加ボタン

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,46 @@
-import React from "react";
-import './App.css';
+import React, { useState } from "react";
+import classes from "./App.module.scss";
 
 export const App = () => {
+  const [addText, setText] = useState("");
+  const [addBokeText, setBokeText] = useState([""]);
+
+  // テキストの追加
+  const onChangeAddText = (event) => setText(event.target.value);
+
+  // テキスト追加後
+  const onClickBokeButton = () => {
+    const newText = [...addBokeText, addText]
+    setBokeText(newText);
+  };
+
+
   return (
     <>
-      <div className="boke">
+      <div>
         <ul>
-          <button>Delete</button>
-          <li>bbb</li>
+          {addBokeText.map((text) => {
+            return (
+            <div className={classes.flex}>
+              <button className={classes.button}>Delete</button>
+              <li>{text}</li>
+            </div>
+            );
+          })}
         </ul>
       </div>
       <div>
         <ul>
-          <li>aaa</li>
-          <button>Delete</button>
+          <div className={classes.flex}>
+            <li>aaa</li>
+            <button className={classes.button}>Delete</button>
+          </div>
         </ul>
       </div>
       <div>
-        <button>Add</button>
-        <input placeholder="Add Text"></input>
-        <button>Add</button>
+        <button className={classes.button}>Add</button>
+        <input placeholder="Add Text" value={addText} onChange={onChangeAddText}></input>
+        <button className={classes.button} onClick={onClickBokeButton}>Add</button>
       </div>
     </>
   );
